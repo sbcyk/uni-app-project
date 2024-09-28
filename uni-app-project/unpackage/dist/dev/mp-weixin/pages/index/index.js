041bb9cb0af5b8f2f3c540e90dff42630b94f549
cyk <sdlxcyk@outlook.com>
2024-09-28 14:16:13 +0800

    commit

---
+++ uni-app-project/unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -0,0 +1,26 @@
+"use strict";
+const common_vendor = require("../../common/vendor.js");
+const common_assets = require("../../common/assets.js");
+const _sfc_main = {
+  data() {
+    return {
+      title: "有点卡卡的aaa"
+    };
+  },
+  onLoad() {
+    this.fuck();
+  },
+  methods: {
+    fuck() {
+      console.log("xixixi fuuuuuuuuuuuuuuuk");
+    }
+  }
+};
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return {
+    a: common_assets._imports_0,
+    b: common_vendor.t($data.title)
+  };
+}
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
+wx.createPage(MiniProgramPage);
